81229ee6ec35feb59541da7ca70bff20172af74d
Muhammad Talha <talhahadees786@gmail.com>
2020-03-23 16:26:08 +0500

    How to build your own promise

--- src/components/BuildingYourOwnPromise.jsx
+++ src/components/BuildingYourOwnPromise.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
-const myPromise = new Promise((resolve, reject) => {
-    console.log("This is answer")
-});
+// Example : 
 
-myPromise.then((data) => {
-    console.log("Hello")
-    console.log(data)
-})
+// const p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("Promise Resolve")
+//     }, 1000)
+//     setTimeout(() => {
+//         reject("Promise Reject")
+//     }, 1000)
+// })
+
+// p.then(data => {
+//     console.log(data) // Answer = Promise Resolve
+// })
+//     .catch(err => {
+//         console.log(err) // Answer = Promise Reject
+//     })
 
 const BuildMyOwnPromise = () => {
     return (
